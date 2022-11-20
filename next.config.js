814596fc32444d6256502669d7fc3f6dab8b2dbe
root <root@DESKTOP-FO21HTU>
2022-11-20 03:57:46 -0600

    pageExtensions fix

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '',
+  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js']
 }
 
 module.exports = nextConfig
