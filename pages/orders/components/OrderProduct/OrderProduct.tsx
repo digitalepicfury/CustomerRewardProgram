6ca8f5d50a11f1bd9296b5c7de5bbf8f62f9bb3c
root <root@DESKTOP-FO21HTU>
2022-11-20 02:30:04 -0600

    Lint Fixes

--- pages/orders/components/OrderProduct/OrderProduct.tsx
+++ pages/orders/components/OrderProduct/OrderProduct.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 
 import _ from "lodash";
 import { v4 as uuidv4 } from 'uuid';
--- pages/rewards/components/RewardModal/components/OrderList/OrderList.tsx
+++ pages/rewards/components/RewardModal/components/OrderList/OrderList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 import { IOrder } from '@models';
 import { OrderListItem } from '..';
@@ -6,7 +7,7 @@ import { OrderListItem } from '..';
 export default function OrderList({ orders }: { orders: IOrder[] }) {
     return (
     <>
-        { orders.map((order: IOrder) => <OrderListItem order={order} rewardAmount={order.reward ? order.reward : 0} />) }
+        { orders.map((order: IOrder) => <OrderListItem key={uuidv4()} order={order} rewardAmount={order.reward ? order.reward : 0} />) }
     </>
   );
 }
